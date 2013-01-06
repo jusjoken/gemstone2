e9e7e6ba0cc0da7dd1cb7a69fb35a46ac9c9702d
usjusjoken <usjusjoken@gmail.com>
2013-01-06 18:36:35 +0000

    Version number change only - version 1.0201

--- Gemstone/src/Gemstone/api.java
+++ Gemstone/src/Gemstone/api.java
@@ -22,7 +22,7 @@ public class api {
 
     public static Logger LOG=Logger.getLogger(api.class);
 
-    public static String Version = "1.0191" + "";
+    public static String Version = "1.0201" + "";
     private static boolean STVAppStarted = false;
     private static boolean LoadCompleted = false;
     
